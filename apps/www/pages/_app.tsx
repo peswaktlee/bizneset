6663e084760f79033847dd9bb80963e39ad734b7
Benjamin <bfzli@hotmail.com>
2024-12-01 14:27:07 +0100

    @bfzli | Update webpack config, package dependencies, and file paths
    - Updated webpack config to include .ts and .tsx extensions
    - Removed tailwindcss from package.json dependencies
    - Refactored file paths in _app.tsx and index.tsx
    - Deleted reactflow.css styling file
    - Updated devDependencies in package-lock.json

--- apps/www/pages/_app.tsx
+++ apps/www/pages/_app.tsx
@@ -1,6 +1,5 @@
-import '../ui/styles/tailwind.css'
-import '../ui/styles/modulify.css'
-import '../ui/styles/reactflow.css'
+import '~/ui/styles/tailwind.css'
+import '~/ui/styles/bizneset.css'
 
 import { FC, ReactNode } from 'react'
 import { AppProps } from 'next/app'
