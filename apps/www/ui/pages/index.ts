d583e0319e981af1550fecdcdb58d28b8f5ac335
Benjamin <bfzli@hotmail.com>
2024-12-01 16:54:15 +0100

    @bfzli | Refactor build scripts and add new error pages
    - Deleted unnecessary config files
    - Updated package.json scripts for building and starting the app
    - Added Error and NotFound pages to UI module

--- apps/www/ui/pages/index.ts
+++ apps/www/ui/pages/index.ts
@@ -1,9 +1,13 @@
 import Account from '@/ui/pages/Account'
 import Business from '@/ui/pages/Business'
 import Businesses from '@/ui/pages/Businesses'
+import Error from '@/ui/pages/Error'
+import NotFound from '@/ui/pages/NotFound'
 
 export {
     Account,
     Business,
-    Businesses
+    Businesses,
+    Error,
+    NotFound
 }
