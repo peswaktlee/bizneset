71caee2baf098a98b5995f089b84edae9ec79c3e
Benjamin <bfzli@hotmail.com>
2024-12-01 14:31:44 +0100

    @bfzli | Update Tailwind config content path
    Changed the content path in the Tailwind config to target specific directory for JSX and TSX files.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
     content: [
-        './**/*.{jsx,tsx}'
+        './apps/www/**/*.{jsx,tsx}'
     ],
     options: {},
     theme: {}
